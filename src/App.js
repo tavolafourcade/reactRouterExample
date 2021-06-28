9a8837dddff70f87c3ef4ae0e4ca1dea7687cd9d
tavolafourcade <tavolafourcade@gmail.com>
2021-06-28 13:33:30 -0500

    Connecting to the paths as a Single Page Application and Implementing Link to each path

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import { Link, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Link to='/'>Home</Link><br/>
+      <Link to='/about'>About</Link><br/>
+      <Link to='/contact'>Contact</Link><br/>
+      
+      <Route path='/' exact component={Home}/>
+      <Route path='/about' component={About}/>
+      <Route path='/contact' component={Contact}/>
     </div>
   );
 }
