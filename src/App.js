99d13d3c1ab7d547be3f655121cec2ee0a8c146f
tavolafourcade <tavolafourcade@gmail.com>
2021-06-29 23:22:55 -0500

    Implemented 404 Not Found page using Switch component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import { Link, Route } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Error404 from './components/Error404'
 
 function App() {
   return (
@@ -12,10 +13,14 @@ function App() {
       <Link to='/'>Home</Link><br/>
       <Link to='/about'>About</Link><br/>
       <Link to='/contact'>Contact</Link><br/>
-
-      <Route path='/' exact component={Home}/>
-      <Route path='/about' component={About}/>
-      <Route path='/contact' component={Contact}/>
+      <Link to='/Error404'>Not Found</Link><br/>
+      <Switch>
+        <Route path='/' exact component={Home}/>
+        <Route path='/about' component={About}/>
+        <Route path='/contact' component={Contact}/>
+        <Route component={Error404}/>
+      </Switch>
+      
     </div>
   );
 }
